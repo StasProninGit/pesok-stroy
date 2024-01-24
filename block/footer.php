409906ebf0123d36a5a284f4bf01d062e1ec1088
Stas <staspronin@mail.ru>
2024-01-24 17:15:59 +0300

    24/01 17-15

---
+++ block/footer.php
@@ -0,0 +1,95 @@
+
+
+<footer class="footer">
+    <div class="footer__wrapper">
+      <div class="container">
+        <div class="footer__content">
+          <div class="footer__top">
+            <div class="footer__logo">
+              <div class="footer__logo__img">
+
+              </div>
+              <div class="footer__logo__text">
+                Купить строительный песок в<br> Москве - компания Песок-Строй
+              </div>
+
+            </div>
+            <div class="footer__catalog">
+              <h3 class="footer__catalog__title">Каталог</h3>
+              <ul class="footer__catalog__list list-reset">
+                <li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="">Песок</a></li>
+                <li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="">Щебень</a></li>
+                <li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="">Грунт</a></li>
+                <li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="">Керамзит</a></li>
+                <li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="">Щебеночно-песчаная смесь</a></li>
+
+              </ul>
+
+            </div>
+            <div class="footer__form">
+              <h3 class="footer__form__title">Остались вопросы?</h3>
+              <p class="footer__form">Заполните форму и наши специалисты<br> свяжутся с Вами в ближайшее время.</p>
+              <form action="#" class="footer__form__content">
+
+                <!-- Каждый input для выравнивания вкладываем в блок input-box -->
+                <div class="input-box">
+                  <input type="text" placeholder="Ваше имя" />
+                </div>
+                <!-- <div class="input-box">
+                <input type="text" placeholder="Введите email" />
+                </div> -->
+                <div class="input-box">
+                  <input type="text" placeholder="Введите телефон" />
+                </div>
+                <div class="button">
+                  <input type="button" value="Отправить" />
+                </div>
+                <div class="footer__checkbox">
+                  <input type="checkbox" id="happy" name="happy" value="yes">
+                  <label for="happy">Соглашаюсь с обработкой своих персональных данных</label>
+                </div>
+
+
+              </form>
+
+
+
+            </div>
+            <div class="footer__contact">
+              <h3 class="footer__catalog__title">Контакты</h3>
+              <ul class="footer__contact__list list-reset">
+                <li class="footer__contact__list__item">
+                  <i class="fa fa-location-arrow" aria-hidden="true"></i><span>г.Москва, 3-й угрешский проезд 6 стр
+                    1</span>
+                </li>
+                <li class="footer__contact__list__item"><i class="fa fa-mobile" aria-hidden="true"></i><a href="#">+7
+                    999 999 99 99</a></li>
+                <li class="footer__contact__list__item"><i class="fa fa-envelope" aria-hidden="true"></i><a
+                    href="#">info@pesok-stroy.ru</a></li>
+              </ul>
+
+            </div>
+
+          </div>
+
+          <div class="footer__bottom">
+            ООО "Песок-Строй" 2018-2024 Все права защищены.
+
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+
+
+
+  </footer>
+
+
+  <script src="https://kit.fontawesome.com/fce9a50d02.js" crossorigin="anonymous"></script>
+  <script src="https://api-maps.yandex.ru/2.1/?apikey=ваш API-ключ&lang=ru_RU"></script>
+  <script src="/js/script.js"></script>
---
+++ block/header.php
@@ -0,0 +1,148 @@
+<header class="header">
+    <div class="header__top">
+      <div class="container">
+        <div class="header__top__content">
+          <div class="header__top__left">
+            Поставка нерудных материалов
+          </div>
+          <div class="header__top__right">
+            <ul class="header__top__right__list list-reset">
+              <li class="hvr-underline-reveal"><a href="/pages/aboutcompani.html">О компании</a></li>
+              <li class="hvr-underline-reveal"><a href="#">Цены</a></li>
+              <li class="hvr-underline-reveal"><a href="#">Доставка и Оплата</a></li>
+              <li class="hvr-underline-reveal"><a href="#">Отзывы</a></li>
+              <li class="hvr-underline-reveal"><a href="#">Вопрос-Ответ</a></li>
+              <li class="hvr-underline-reveal"><a href="/pages/contact.html">Контакты</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div class="header__middle">
+      <div class="container">
+        <div class="header__middle__content">
+
+          <div class="header__middle__logo">
+
+          </div>
+
+          <div class="header__middle__time">
+            <div class="slogan">
+              <i class="fa fa-clock-o" aria-hidden="true"></i>
+              <span>
+                Работа офиса с 9 до 23
+              </span>
+            </div>
+            <div class="slogan">
+              <i class="fa fa-truck" aria-hidden="true"></i>
+              <span>
+                График работы 24/7
+              </span>
+            </div>
+
+          </div>
+
+          <div class="header__middle__call">
+            <div class="header__link">
+              <i class="fa fa-headphones" aria-hidden="true"></i>
+              <a href="#">Обратный звонок</a>
+            </div>
+            <div class="header__link">
+              <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+              <a href="#">Скачать прайс лист</a>
+            </div>
+            <div class="header__link">
+              <i class="fa fa-whatsapp" aria-hidden="true"></i>
+              <a href="#">Написать на WhatSapp</a>
+            </div>
+
+          </div>
+
+          <div class="header__middle__contact">
+            <div class="header__middle__contact__phone">
+              <i class="fa fa-phone-square" aria-hidden="true"></i>
+              <a href="tel:+">+7(985)777-77-77</a>
+            </div>
+            <div class="header__middle__contact__mail">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <a href="mailto:">pesok-stroy@mail.ru</a>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="header__nav">
+      <div class="container">
+        <div class="header__nav__content">
+
+
+          <ul class="topmenu">
+            <li><i class="fa fa-cog fa-spin"></i><a href="" class="submenu-link">ПЕСОК</a>
+              <ul class="submenu">
+                <li><a href="">Производство</a></li>
+                <li><a href="">Магазин</a></li>
+                <li><a href="">Сервис</a></li>
+              </ul>
+            </li>
+            <li><i class="fa fa-cog fa-spin"></i><a href="" class="submenu-link">ЩЕБЕНЬ</a>
+              <ul class="submenu">
+                <li><a href="">Производство</a></li>
+                <li><a href="">Магазин</a></li>
+                <li><a href="">Сервис</a></li>
+              </ul>
+            </li>
+
+            <li><i class="fa fa-cog fa-spin"></i><a href="" class="submenu-link">ГРУНТ</a>
+              <ul class="submenu">
+                <li><a href="">Производство</a></li>
+                <li><a href="">Магазин</a></li>
+                <li><a href="">Сервис</a></li>
+              </ul>
+            </li>
+
+            <li><i class="fa fa-cog fa-spin"></i><a href="" class="submenu-link">РЕЦИКЛ</a>
+              <ul class="submenu">
+                <li><a href="">Производство</a></li>
+                <li><a href="">Магазин</a></li>
+                <li><a href="">Сервис</a></li>
+              </ul>
+            </li>
+
+            <li><i class="fa fa-cog fa-spin"></i><a href="" class="submenu-link">АСФАЛЬТ</a>
+              <ul class="submenu">
+                <li><a href="">Производство</a></li>
+                <li><a href="">Магазин</a></li>
+                <li><a href="">Сервис</a></li>
+              </ul>
+            </li>
+
+            <li><i class="fa fa-cog fa-spin"></i><a href="" class="submenu-link">ПЕРЕВОЗКА</a>
+              <ul class="submenu">
+                <li><a href="">Производство</a></li>
+                <li><a href="">Магазин</a></li>
+                <li><a href="">Сервис</a></li>
+              </ul>
+            </li>
+
+          </ul>
+
+
+        </div>
+
+      </div>
+
+    </div>
+
+
+
+  </header>
+
+
+
