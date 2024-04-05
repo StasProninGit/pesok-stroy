18a1495ee227e7a8e2bdd31e1301503fe48dcf50
Stas <staspronin@mail.ru>
2024-04-05 19:00:33 +0300

    0504

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
 // map
 
 
+ymaps.ready(init);
 function init() {
-  var map = new ymaps.Map("map", {
-    center: [55.76, 37.64],
-    zoom: 8
-  });
+	var myMap = new ymaps.Map("map", {
+		center: [55.76, 37.64],
+		zoom: 8
+	}, {
+		searchControlProvider: 'yandex#search'
+	});
 
-  let placemark = new ymaps.Placemark([55.65997652358045,36.74867991406251], {}, {
-    iconLayout: 'default#image',
-    iconImageHref: 'https://cdn-icons-png.flaticon.com/512/9218/9218033.png',
-    iconImageSize: [30, 30],
-    iconImageOffset: [0, 0]
+  var myPlacemark = new ymaps.Placemark([[55.643378746520476,36.648086347167975]], {
+		iconContent: 'Карьер'
+	}, {
+		preset: 'islands#darkOrangeStretchyIcon'
+	});
+	myMap.geoObjects.add(myPlacemark);
 
-  });
 
-  // let placemark = new ymaps.Placemark([55.41906756739088,37.580891815718296], {}, {
-  //   iconLayout: 'default#image',
-  //   iconImageHref: 'https://cdn-icons-png.flaticon.com/512/9218/9218033.png',
-  //   iconImageSize: [30, 30],
-  //   iconImageOffset: [0, 0]
 
-  map.controls.remove('geolocationControl'); // удаляем геолокацию
-  map.controls.remove('searchControl'); // удаляем поиск
-  map.controls.remove('trafficControl'); // удаляем контроль трафика
-  map.controls.remove('typeSelector'); // удаляем тип
-  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+
+
+
+
+
+
+  myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+  myMap.controls.remove('searchControl'); // удаляем поиск
+  myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+  myMap.controls.remove('typeSelector'); // удаляем тип
+  myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
   // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-  map.controls.remove('rulerControl'); // удаляем контрол правил
-  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  myMap.controls.remove('rulerControl'); // удаляем контрол правил
+  myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 
-  map.geoObjects.add(placemark);
 }
 
-ymaps.ready(init);
+// myMap.ready(init);
